ea2c709ecbc8d561d4c99c4a8e35b3252baafc02
HussienAbdelaal <hussienabdelaal105@gmail.com>
2019-09-29 13:16:31 +0200

    fix client routing issue

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -4,12 +4,12 @@ import { Subject, BehaviorSubject } from 'rxjs';
 
 const httpOptionsJson = {
   headers: new HttpHeaders({
-    'Content-Type': 'text/plain',
-    'Accept': 'text/plain'
+    'Authorization': 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDhmYmE3YjE0ZDgxNDMwNzI4ZTdiNGQiLCJuYW1lIjoiZXhhbXBsZSIsImVtYWlsIjoiZXhhbXBsZUBleGFtcGxlLmNvbSIsInVzZXJuYW1lIjoiZXhhbXBsZSIsInBhc3N3b3JkIjoiJDJhJDEwJEdTRzFFTkptYkhCWWhlTlZxeWZ4MnVEZTQvZTRZSnlUNk94eFpXSFVpdjlGUGs4blRtV00yIiwiX192IjowLCJpYXQiOjE1Njk3MDA1NDJ9.qE1KgQ0bOGmkPUseyhVFSKTBAy_e1J3hKFmFELvdR_c'
   }),
 };
 
-const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Accept': 'application/json' });
+const headers = new HttpHeaders({'Authorization': 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXIiOiIvcGF0aC90by9nYWxsZXJ5L2RlZmF1bHRBdmF0YXIucG5nIiwicm9sZSI6ImFkbWluIiwiX2lkIjoiNWQ5MDgyODFhNTkzMjU3ODcyNGQ4MTBmIiwibmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMTAkckFoUDQuNzVSL3NMMUlQZEFZaUtRLlFMODVDZlBWUDhDVWxLLndpOGpPYm1ybTlUck9VU0ciLCJfX3YiOjAsImlhdCI6MTU2OTc1NTYyMn0.wI2mPyJuG4sUacWzEbcgLe5zrpa9kQEvrG7Oeyko_BE'
+});
 
 const baseURL = `http://localhost:3000`;
 const queryAllCarsURL = `/fabcar`;
