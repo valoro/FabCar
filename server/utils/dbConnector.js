e01709f587def7f92519114f9c46bec3e2992fd6
HussienAbdelaal <hussienabdelaal105@gmail.com>
2019-11-24 13:49:51 +0200

    moved credintials to .env file

--- server/utils/dbConnector.js
+++ server/utils/dbConnector.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 module.exports = {
-  uri: 'mongodb://valoroisl33t:valoroisl33t@ds215759.mlab.com:15759/fabcar-on-valoro',
-  secret: 'valoroisl33t'
+  uri: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds215759.mlab.com:15759/${process.env.DB_NAME}`,
+  secret: `${process.env.DB_SECRET}`
 };
